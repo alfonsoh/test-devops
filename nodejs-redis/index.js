77291543e1854e576f16db862d14c29d511a1d4b
alfonsoh <admin@administrator.local>
2020-03-08 09:12:14 -0700

    changing text and site visit counter

--- nodejs-redis/index.js
+++ nodejs-redis/index.js
@@ -13,7 +13,7 @@ client.set('visits', 0);
 //defining the root endpoint
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
-        res.send('Number of visits is: ' + visits + 1)
+        res.send('You just hollered at me: ' + visits)
         client.set('visits', parseInt(visits) + 1)
     })
 })
